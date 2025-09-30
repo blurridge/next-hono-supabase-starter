d28d30d2f1969068511c0a356040cb7692b57445
Zach Riane Machacon <zachriane01@gmail.com>
2025-10-01 00:54:21 +0800

    [Feat][Zach] Add API response creator utility

---
+++ src/utils/createResponse.ts
@@ -0,0 +1,25 @@
+export const RESPONSE_STATUS = {
+  SUCCESS: 'success',
+  FAIL: 'fail',
+} as const;
+
+export type ResponseStatus =
+  (typeof RESPONSE_STATUS)[keyof typeof RESPONSE_STATUS];
+
+export type ResponseType<T = unknown> = {
+  status: ResponseStatus;
+  message: string;
+  data: T | null;
+};
+
+export const createResponse = <T>(
+  status: ResponseStatus,
+  message: string,
+  data: T | null = null
+): ResponseType<T> => {
+  return {
+    status,
+    message,
+    data,
+  };
+};
