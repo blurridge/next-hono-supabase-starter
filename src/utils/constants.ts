b9a918aad706e11c746173c25c15d96f02f81a90
Zach Riane Machacon <zachriane01@gmail.com>
2025-10-01 00:54:54 +0800

    [Feat][Zach] Add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+// Add your application constants here
+// Example:
+// export const APP_NAME = 'My App';
+// export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+
+export const SOMETHING = 'CONSTANT';
