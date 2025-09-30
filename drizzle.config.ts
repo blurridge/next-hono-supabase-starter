90dcf3d67a6075fbd2a7d2cda3b83e9211bbfd32
Zach Riane Machacon <zachriane01@gmail.com>
2025-10-01 00:50:55 +0800

    [Feat][Zach] Add Drizzle configuration file

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema/index.ts',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  schemaFilter: ['public'],
+  entities: {
+    roles: {
+      provider: 'supabase',
+    },
+  },
+});
